eb4edc1721a541c721ec9d23b757bd78262ec1ac
MattCordell <matt@snoyowie.com>
2017-11-16 23:00:05 +1000

    Clean up using references

--- Content-For-Promotion-Extractor/Classes/RF2_Classes.cs
+++ Content-For-Promotion-Extractor/Classes/RF2_Classes.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Content_For_Promotion_Extractor
 {
--- Content-For-Promotion-Extractor/Program.cs
+++ Content-For-Promotion-Extractor/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO.Compression;
-using System.IO;
+
 
 namespace Content_For_Promotion_Extractor
 {
